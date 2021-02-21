bfb3871ef18442faf4f1a9ebb4983a4b90fb0aea
zhangzhenghe <zhangzhhe@gmail.com>
2021-02-21 19:01:46 +0800

    轮播图组件鼠标操作

--- Week 14/carousel/main.js
+++ Week 14/carousel/main.js
@@ -16,27 +16,72 @@ class Carousel extends Component {
       child.style.backgroundImage = `url('${r}')`;
       this.root.appendChild(child)
     }
-    let currentIndex = 0;
-    setInterval(() => {
+
+    let position = 0;
+    this.root.addEventListener('mousedown', (event) => {
+      console.log('mousedown');
       let children = this.root.children;
-      let nextIndex = (currentIndex + 1) % children.length;
-      
-      let current = children[currentIndex]
-      let next = children[nextIndex]
-      
-      next.style.transition = 'none'
-      next.style.transform = `translateX(${100 - nextIndex * 100}%)`;
-      
-      setTimeout(() => {
-        next.style.transition = ''
-        current.style.transform = `translateX(${-100 - currentIndex * 100}%)`;
+      let startX = event.clientX;
+      let move = (event) => {
+        let x = event.clientX - startX;
         
-        next.style.transform = `translateX(${-nextIndex * 100}%)`;
-        currentIndex = nextIndex;
-      },16)
+        let current = position - ((x - x % 500) / 500);
+        for (let offset of [-1, 0, 1]) {
+          let pos = current + offset
+          pos = (pos + children.length) % children.length;
+
+          children[pos].style.transition = 'none'
+          children[pos].style.transform =  `translateX(${- pos * 500 + offset * 500 + x}px)`;
+        }
+      }
+      let up = (event) => {
+        console.log('up')
+        let x = event.clientX - startX;
+        position = position - Math.round(x / 500);
+
+        for (let offset of [0, -Math.sign(Math.round(x / 500) -x +250 * Math.sign(x))]) {
+          let pos = position + offset;
+          pos = (pos + children.length) % children.length;
+          
+          children[pos].style.transition = ''
+          children[pos].style.transform = `translateX(${- pos * 500 + offset * 500}px)`;
+        }
+        
+        // for (let child of children) {
+        //   child.style.transition = ''
+        //   child.style.transform = `translateX(${- position * 500}px)`;
+        // }
+
+        document.removeEventListener('mousemove', move)
+        document.removeEventListener('mouseup', up)
+      }
+
+      document.addEventListener('mousemove', move)
+      document.addEventListener('mouseup', up)
+    })
+
+
+
+    // let currentIndex = 0;
+    // setInterval(() => {
+    //   let children = this.root.children;
+    //   let nextIndex = (currentIndex + 1) % children.length;
       
+    //   let current = children[currentIndex]
+    //   let next = children[nextIndex]
       
-    }, 2000);
+    //   next.style.transition = 'none'
+    //   next.style.transform = `translateX(${100 - nextIndex * 100}%)`;
+      
+    //   setTimeout(() => {
+    //     next.style.transition = ''
+    //     current.style.transform = `translateX(${-100 - currentIndex * 100}%)`;
+        
+    //     next.style.transform = `translateX(${-nextIndex * 100}%)`;
+    //     currentIndex = nextIndex;
+    //   },16)
+      
+    // }, 2000);
     return this.root;
   }
   mountTo(parent) {
